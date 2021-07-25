4709729d5bf46a969ba8250249e48fd26e08d2c1
Egor Gorskikh <egorgorskikh@gmail.com>
2021-07-25 10:43:32 +0300

    init: first commit

---
+++ JSONApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONApp
+//
+//  Created by Egor Gorskikh on 25.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
